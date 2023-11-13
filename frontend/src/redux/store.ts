84678a8b8f4c16f6f5fc4107601950ba9941b1d0
harshdeepgill <er.harshdeepgill@gmail.com>
2023-11-13 19:05:54 +0530

    intrview and list

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { authReducer } from "./authReducer";
 import thunk from "redux-thunk";
+import { aiReducer } from "./aiReducer";
 
 const rootReducer = combineReducers({
-    authReducer
+    authReducer,
+    aiReducer
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
