2a1229f7bef52be67dcc4ae4cacab45eda7423ed
harshdeepgill <er.harshdeepgill@gmail.com>
2023-11-14 22:13:29 +0530

    somewhat done

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -7,7 +7,7 @@ const Signup = () => {
   const navigate = useNavigate()
 
   const [formData, setFormData] = useState({
-    name: "",
+    username: "",
     avatar : "",
     email: "",
     password: "",
@@ -62,16 +62,16 @@ const Signup = () => {
               >
                 <div>
                   <label
-                    htmlFor="name"
+                    htmlFor="username"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Your Name
                   </label>
                   <input
                     type="text"
-                    name="name"
-                    id="name"
-                    value={formData.name}
+                    name="username"
+                    id="username"
+                    value={formData.username}
                     onChange={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Name Surname"
@@ -88,7 +88,7 @@ const Signup = () => {
                   <input
                     type="text"
                     name="avatar"
-                    id="email"
+                    id="avatar"
                     value={formData.avatar}
                     onChange={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
